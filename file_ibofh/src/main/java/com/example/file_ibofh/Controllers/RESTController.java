7384b08ad78cc0c8ae28088f1bf36179f4b4683f
Kartashev Matvey <151167860+ibofh@users.noreply.github.com>
2024-03-22 19:11:53 +0500

    Add files via upload

--- file_ibofh/src/main/java/com/example/file_ibofh/Controllers/RESTController.java
+++ file_ibofh/src/main/java/com/example/file_ibofh/Controllers/RESTController.java
@@ -5,19 +5,78 @@ import java.io.*;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 
 @RestController
 public class RESTController {
     @GetMapping("/getUsers")
-    public ArrayList<Users> getUsers(){
+    public ArrayList<Users> getUsers(@RequestParam(required = false) String searchedName, @RequestParam(required = false) Integer age,
+                                     @RequestParam(required = false) String paramToSort){
         final JsonMapper map = new JsonMapper();
         try {
-             final ArrayList<Users> listOfUsers = map.readValue(new File("/home/user1/Рабочий стол/userList.json"),
+            int Counter = 0;
+            final ArrayList<Users> listOfUsers = map.readValue(new File("src/Utils/userList.json"),
                     new TypeReference<ArrayList<Users>>() {});
-            return listOfUsers;
+            final ArrayList<Users> listToReturn = new ArrayList<>();
+            if (searchedName != null){
+                for (Users user : listOfUsers){
+                    if (user.getName().contains(searchedName)){
+                        listToReturn.add(user);
+                    }
+                }
+                Counter++;
+            }
+            if(Counter == 0){
+                for (Users user : listOfUsers){
+                    listToReturn.add(user);
+                }
+            }
+            if(age != null){
+                ArrayList<Users> buffferList = new ArrayList<>();
+                for(Users user : listToReturn){
+                    if(user.getAge().equals(age)) {
+                        buffferList.add(user);
+                    }
+                }
+                listToReturn.clear();
+                for (Users user : buffferList){
+                    listToReturn.add(user);
+                }
+                Counter++;
+            }
+            if(Counter == 0){
+                for (Users user : listOfUsers){
+                    listToReturn.add(user);
+                }
+            }
+            if(paramToSort != null){
+                HashMap<String, Integer> mapForSort = new HashMap<>();
+                ArrayList<String> bufferListNames = new ArrayList<>();
+                ArrayList<Integer> bufferListAges = new ArrayList<>();
+                ArrayList<Users> bufferListToReturn = new ArrayList<>();
+                for (Users user : listToReturn){
+                    mapForSort.put(user.getName(), user.getAge());
+                    bufferListNames.add(user.getName());
+                    bufferListAges.add(user.getAge());
+                }
+                switch (paramToSort){
+                    case ("nameUp"):
+                        Collections.sort(bufferListNames);
+                        for (String Name : bufferListNames){
+                            bufferListToReturn.add()
+                        }
+                }
+            }
+
+            return listToReturn;
+
         }catch (IOException e){
             System.err.println(e);
         }
@@ -29,7 +88,7 @@ public class RESTController {
         final JsonMapper map = new JsonMapper();
         ArrayList<Users> newList = new ArrayList<>();
         try {
-            newList = map.readValue(new File("/home/user1/Рабочий стол/userList.json"),
+            newList = map.readValue(new File("src/Utils/userList.json"),
                     new TypeReference<ArrayList<Users>>() {});
         }catch (IOException e){
             System.err.println(e);
@@ -43,7 +102,7 @@ public class RESTController {
             newList.remove(index);
         }
         try{
-            map.writeValue(new File("/home/user1/Рабочий стол/userList.json"),
+            map.writeValue(new File("src/Utils/userList.json"),
                     newList);
         }catch (IOException e){
             System.err.println(e);
@@ -51,29 +110,29 @@ public class RESTController {
 
     }
     @PostMapping("/addUser")
-    public void addUser(@RequestParam("name") String name, @RequestParam("age") int age){
+    public void addUser(@RequestBody Users user){
         boolean flag = false;
         Users userToAdd = new Users();
         final JsonMapper map = new JsonMapper();
         ArrayList<Users> newList = new ArrayList<>();
         try {
-            newList = map.readValue(new File("/home/user1/Рабочий стол/userList.json"),
+            newList = map.readValue(new File("src/Utils/userList.json"),
                     new TypeReference<ArrayList<Users>>() {});
         }catch (IOException e){
             System.err.println(e);
         }
         for (int i = 0; i < newList.size();i++ ){
-            if (name.equals(newList.get(i).getName())){
+            if (user.getName().equals(newList.get(i).getName())){
                 flag = true;
             }
         }
         if(flag != true) {
-            userToAdd.setAge(age);
-            userToAdd.setName(name);
+            userToAdd.setAge(user.getAge());
+            userToAdd.setName(user.getName());
             newList.add(userToAdd);
         }
         try{
-            map.writeValue(new File("/home/user1/Рабочий стол/userList.json"),
+            map.writeValue(new File("src/Utils/userList.json"),
                     newList);
         }catch (IOException e){
             System.err.println(e);
@@ -81,30 +140,30 @@ public class RESTController {
 
     }
     @PutMapping("/updateUser")
-    public void updateUser(@RequestParam("name") String name, @RequestParam("age") int age){
+    public void updateUser(@RequestBody Users user){
         int index = -1;
         Users userToAdd = new Users();
         final JsonMapper map = new JsonMapper();
         ArrayList<Users> newList = new ArrayList<>();
         try {
-            newList = map.readValue(new File("/home/user1/Рабочий стол/userList.json"),
+            newList = map.readValue(new File("src/Utils/userList.json"),
                     new TypeReference<ArrayList<Users>>() {});
         }catch (IOException e){
             System.err.println(e);
         }
         for (int i = 0; i < newList.size();i++ ){
-            if (name.equals(newList.get(i).getName())){
+            if (user.getName().equals(newList.get(i).getName())){
                 index = i;
             }
         }
         if(index != -1) {
             newList.remove(index);
-            userToAdd.setName(name);
-            userToAdd.setAge(age);
+            userToAdd.setName(user.getName());
+            userToAdd.setAge(user.getAge());
             newList.add(userToAdd);
         }
         try{
-            map.writeValue(new File("/home/user1/Рабочий стол/userList.json"),
+            map.writeValue(new File("src/Utils/userList.json"),
                     newList);
         }catch (IOException e){
             System.err.println(e);
--- file_ibofh/src/test/java/com/example/file_ibofh/FileIbofhApplicationTests.java
+++ file_ibofh/src/test/java/com/example/file_ibofh/FileIbofhApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.file_ibofh;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FileIbofhApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(FileIbofhApplicationTests.class, args);
 	}
 
 }
